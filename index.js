5f955442f64545687d90d7dbfcfaaf61136652ec
Bittmen <38054257+Bittmen@users.noreply.github.com>
2018-11-17 11:53:37 -0600

    'update'

--- index.js
+++ index.js
@@ -4,36 +4,49 @@ var  PORT = 1234;
 const index = require('./js/main')
 let data = "Hola";
 
-net.createServer(function(sock) {
+//Se crea el servidor
+const server = net.createServer(function(sock) {
 
   console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
 
   var sockets = [];
 
   sock.on('data', function(data) {
-     console.log("DATA: "+data)
+     console.log("DATA: "+data);
   });
 
-sock.on('close', function(data){
+  sock.on('close', function(data){
     console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
-});
+  });
 
 }).listen(PORT, HOST, function(){
     console.log("listening on port: "+PORT+" and host: "+HOST);
 });
 
-index.server.listen(PORT = 3000, () => {
-    console.log(`Server running in http://localhost:${PORT}`)
- });
-
-index.io.on('connection', function(socket){
-    console.log(`client: ${socket.id}`)
-   
-      socket.emit('sending', data)
- 
-    //Reciviendo algo del cliente
-    socket.on('evento', (num) => {
-      console.log(num);
+//Una vez que escucha el servidor TCP se crea el servidor Web
+server.addListener('listening', ()=>{
+    index.server.listen(port = 3000, () => {
+        console.log(`Server running in http://localhost:${port}`)
+     });
+    
+    index.io.on('connection', function(socket){
+        console.log(`client: ${socket.id}`)
+       
+        setInterval(function(){
+            socket.emit('sending', data);
+        }, 1000);
+          
+        //Reciviendo algo del cliente
+        socket.on('evento', (num) => {
+          console.log(num);
+        });
+    
     });
-
 });
+
+
+
+
+    
+
+
